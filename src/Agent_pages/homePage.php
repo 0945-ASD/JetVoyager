39f396e4e86c7d3b866ba8a123481f42093477ad
0945-ASD <chethaatapattu@gmail.com>
2025-01-05 22:36:28 +0530

    slight more location updates

--- src/Agent_pages/homePage.php
+++ src/Agent_pages/homePage.php
@@ -75,7 +75,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JetVoyager Agent Portal</title>
-    <link rel="stylesheet" href="http://localhost/src/Agent_pages/homePage.css">
+    <link rel="stylesheet" href="http://localhost/JetVoyager/src/Agent_pages/homePage.css">
     <script src="scripts.js" defer></script>
 </head>
 
